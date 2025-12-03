62b2f7650a4f3d4e5b99336ced32baa1149c1e68
Riccardo de Lutio <rdelutio@nvidia.com>
2025-12-03 10:36:55 -0800

    add comment

--- threedgrut/render.py
+++ threedgrut/render.py
@@ -61,6 +61,7 @@ class Renderer:
         """Create the test dataloader for the given configuration."""
         from threedgrut.datasets.utils import configure_dataloader_for_platform
 
+        # test mode to ensure we render all the images regardless of the selected indices in ColmapDataset
         dataset = datasets.make_test(name=conf.dataset.type, config=conf)
 
         # Configure DataLoader arguments for the current platform
